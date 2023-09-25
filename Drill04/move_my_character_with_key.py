8ae029ca4e21015228dd8309f0332e229dded5b9
Lee Hye Bin <sasg6780@naver.com>
2023-09-25 17:41:37 +0900

    Drill04 제출(2020180050 이혜빈)

--- Drill04/move_my_character_with_key.py
+++ Drill04/move_my_character_with_key.py
@@ -6,7 +6,7 @@ TUK_WIDTH, TUK_HEIGHT = 1100, 800
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 
-character = load_image('animation_girl.png')
+character = load_image('animation_girls.png')
 
 
 def handle_events():
