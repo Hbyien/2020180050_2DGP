33dd82d45c867fae91b17b518e1087f806bd1b35
Lee Hye Bin <sasg6780@naver.com>
2023-09-27 20:03:11 +0900

    Drill05 (2020180050 이혜빈)

--- Drill05/move_character_with_mouse.py
+++ Drill05/move_character_with_mouse.py
@@ -16,19 +16,22 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        # elif event.type == SDL_MOUSEMOTION:
+        #     x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
-def draw_mouse_point(p):
-    x = random.uniform(0, 800)
-    y = random.uniform(0, 600)
+def draw_mouse_point(x, y):
     mouse.draw(x, y)
 
+def character_move(x,y, a, b):
+    global frame
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+
+
 running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+a, b = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hide_cursor()
 
@@ -37,15 +40,20 @@ while running:
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     while True:
-        # x = random.uniform(0, 800)
-        # y = random.uniform(0, 600)
+        handle_events()
+        x,y =a, b
+        a = random.uniform(0, TUK_WIDTH)
+        b = random.uniform(0, TUK_HEIGHT)
         # mouse.draw(x,y)
+        draw_mouse_point(a, b)
+        character_move(x, y,a,b)
+
         delay(0.01)
         update_canvas()
 
-    frame = (frame + 1) % 8
+        frame = (frame + 1) % 8
 
-    handle_events()
+
 
 close_canvas()
 
